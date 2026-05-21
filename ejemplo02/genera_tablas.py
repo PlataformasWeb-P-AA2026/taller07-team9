6a4d24bcd052d985e7ab58fa4d7a5bcc3fc7a4ed
Jeank935 <jccuenca17@utpl.edu.ec>
2026-05-21 12:56:31 -0500

    Ingres1

--- ejemplo02/genera_tablas.py
+++ ejemplo02/genera_tablas.py
@@ -3,20 +3,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy import Column, Integer, String, ForeignKey
 
-# se importa información del archivo configuracion
 from configuracion import cadena_base_datos
 
-# se genera en enlace al gestor de base de
-# datos
-# para el ejemplo se usa la base de datos
-# sqlite
 engine = create_engine(cadena_base_datos)
 
 Base = declarative_base()
-
-# Ejemplo que representa la relación entre dos clases
-# One to Many
-# Un club tiene muchos jugadores asociados
 
 class Club(Base):
     __tablename__ = 'club'
@@ -24,9 +15,6 @@ class Club(Base):
     nombre = Column(String(100))
     deporte = Column(String(100))
     fundacion = Column(Integer, nullable=False)
-    # Mapea la relación entre las clases
-    # Club puede acceder a los jugadores asociados
-    # por la llave foránea
     jugadores = relationship("Jugador", back_populates="club")
 
     
@@ -42,11 +30,8 @@ class Jugador(Base):
     nombre = Column(String(100), nullable=False)
     dorsal = Column(Integer)
     posicion = Column(String(100))
-    # se agrega la columna club_id como ForeignKey
-    # se hace referencia al id de la entidad club
     club_id = Column(Integer, ForeignKey('club.id'))
-    # Mapea la relación entre las clases
-    # Jugador tiene una relación con Club
+
     club  = relationship("Club", back_populates="jugadores")
     
     def __repr__(self):
